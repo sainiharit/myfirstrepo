5bc2fa4d10078b7b106c6eeda430b95f38bdb497
Harit Saini <43183025+sainiharit@users.noreply.github.com>
2020-10-01 21:48:16 +0530

    Error in Loading the Page
    Corrected by changing the core function

--- main.js
+++ main.js
@@ -60,16 +60,7 @@
 	/*  Video          
 	============================================*/
 	var siteMagnificPopup = function() {
-		$('.image-popup').magnificPopup({
-	    type: 'image',
-	    closeOnContentClick: true,
-	    closeBtnInside: false,
-	    fixedContentPos: true,
-	    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-	     gallery: {
-	      enabled: true,
-	      navigateByImgClick: true,
-	      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+
 	    },
 	    image: {
 	      verticalFit: true
@@ -92,6 +83,12 @@
 	};
 	siteMagnificPopup();
 
+      $('.back-to-top').on('click',function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: 0
+        }, 600);
+        return false;
 	
 
     /* Back Top Link active
